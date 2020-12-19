1a83872e3752757afe110133b12bbf23df865f64
VladislavKuntsov <70582102+VladislavKuntsov@users.noreply.github.com>
2020-12-19 14:52:43 +0300

    Creat 1.5

---
+++ js/slider.js
@@ -0,0 +1,81 @@
+var slider = document.querySelector('.swiper-container');
+var mySwiper;
+
+ function mobileSlider() {
+    if (window.innerWidth < 768 && slider.dataset.mobile === 'false') {
+        mySwiper = new Swiper(slider, {
+            slidesPerView: 'auto',
+            spaceBetween: 16,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            }
+        })
+        slider.dataset.mobile = 'true';
+    }
+    if (window.innerWidth >= 768) {
+        slider.dataset.mobile = 'false';
+        mySwiper.destroy();   
+        if (slider.classList.contains('.swiper-container-initialized')) {
+            mySwiper.destroy();
+        }
+    }
+}
+    mobileSlider()
+    window.addEventListener('resize', () => {
+        mobileSlider();
+        showLessMore();
+    });
+
+    var slides = document.querySelectorAll('.swiper-slide');
+    console.log(slides);
+    
+    function showLess() {
+        if(window.innerWidth >= 768 && window.innerWidth < 992) {
+            for(let i = 6; i < slides .length; i++) {
+                slides[i].style.display = "none";
+            }
+        }
+        if(window.innerWidth >= 992) {
+            for(let i = 9; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+        }
+    }
+
+    function showMore   () {
+        if(window.innerWidth >= 768 && window.innerWidth < 992) {
+            for(let i = 7; i < slides .length; i++) {
+                slides[i].style.display = 'block';
+                
+            }
+        }
+        if(window.innerWidth >= 992 ) {
+            for(let i = 9; i < slides.length; i++) {
+                slides[i].style.display = 'block';
+            }
+        }
+    }
+
+    var less = document.querySelector('.button-less');
+    var more = document.querySelector('.button-more');
+
+    function showLessMore () {
+        less.addEventListener('click', function(evt) { 
+            evt.preventDefault();
+            
+            more.style.display = 'block';
+            less.style.display = 'none';
+            showLess();
+        })
+    
+        more.addEventListener('click', function(evt) { 
+            evt.preventDefault();
+            
+            more.style.display = 'none';
+            less.style.display = 'block';
+            showMore ();
+        })
+    }   
+
+    
